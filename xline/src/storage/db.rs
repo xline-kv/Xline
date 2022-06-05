d957822a272fc1461cc24d3c608cd8e0eec3e8fe
Zheng Pan <zhengpan@google.com>
2022-06-04 17:28:30 -0700

    Refactor index and db to separated modules

---
+++ xline/src/storage/db.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+
+//use clippy_utilities::OverflowArithmetic;
+use parking_lot::Mutex;
+
+use super::revision::Revision;
+use crate::rpc::KeyValue;
+
+/// Database to store revision to kv mapping
+#[derive(Debug)]
+pub(crate) struct DB {
+    /// internal storage of `DB`
+    storage: Mutex<HashMap<Revision, KeyValue>>,
+}
+
+impl DB {
+    /// New `DB`
+    pub(crate) fn new() -> Self {
+        Self {
+            storage: Mutex::new(HashMap::new()),
+        }
+    }
+
+    /// Insert a `KeyValue`
+    pub(crate) fn insert(&self, revision: Revision, kv: KeyValue) -> Option<KeyValue> {
+        self.storage.lock().insert(revision, kv)
+    }
+
+    /// Get a `KeyValue`
+    pub(crate) fn get(&self, revision: &Revision) -> Option<KeyValue> {
+        self.storage.lock().get(revision).cloned()
+    }
+
+    /// Get a list of `KeyValue`
+    pub(crate) fn get_values(&self, revisions: &[Revision]) -> Vec<KeyValue> {
+        let storage = self.storage.lock();
+        revisions
+            .iter()
+            .filter_map(|revision| storage.get(revision).cloned())
+            .collect()
+    }
+
+    /// Mark deletion for keys
+    pub(crate) fn mark_deletions(&self, revisions: &[(Revision, Revision)]) -> Vec<KeyValue> {
+        let mut storage = self.storage.lock();
+        let prev_kvs: Vec<KeyValue> = revisions
+            .iter()
+            .map(|&(ref prev_rev, _)| {
+                storage
+                    .get(prev_rev)
+                    .cloned()
+                    .unwrap_or_else(|| panic!("Failed to get revision {:?} from DB", prev_rev))
+            })
+            .collect();
+        assert!(
+            prev_kvs.len() == revisions.len(),
+            "Index doesn't match with DB"
+        );
+        prev_kvs
+            .iter()
+            .zip(revisions.iter())
+            .for_each(|(kv, &(_, new_rev))| {
+                let del_kv = KeyValue {
+                    key: kv.key.clone(),
+                    ..KeyValue::default()
+                };
+                let _prev_val = storage.insert(new_rev, del_kv);
+            });
+
+        prev_kvs
+    }
+}
---
+++ xline/src/storage/revision.rs
@@ -0,0 +1,66 @@
+/// Revison of a key
+#[derive(Debug, Copy, Clone)]
+pub(crate) struct KeyRevision {
+    /// Last creation revision
+    pub(crate) create_revision: i64,
+    /// Number of modification since last creation
+    pub(crate) version: i64,
+    /// Last modification revision
+    pub(crate) mod_revision: i64,
+    /// Sub revision in one transaction
+    pub(crate) sub_revision: i64,
+}
+
+/// Revision
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub(crate) struct Revision {
+    /// Main revision
+    revision: i64,
+    /// Sub revision in one transaction or range deletion
+    sub_revision: i64,
+}
+impl Revision {
+    /// New `Revision`
+    pub(crate) fn new(revision: i64, sub_revision: i64) -> Self {
+        Self {
+            revision,
+            sub_revision,
+        }
+    }
+}
+
+impl KeyRevision {
+    /// New `KeyRevision`
+    pub(crate) fn new(
+        create_revision: i64,
+        version: i64,
+        mod_revision: i64,
+        sub_revision: i64,
+    ) -> Self {
+        Self {
+            create_revision,
+            version,
+            mod_revision,
+            sub_revision,
+        }
+    }
+    /// New a `KeyRevision` to represent deletion
+    pub(crate) fn new_deletion(mod_revision: i64, sub_revision: i64) -> Self {
+        Self {
+            create_revision: 0,
+            version: 0,
+            mod_revision,
+            sub_revision,
+        }
+    }
+
+    /// If current `KeyRevision` represent deletion
+    pub(crate) fn is_deleted(&self) -> bool {
+        self.create_revision == 0 && self.version == 0 && self.sub_revision == 0
+    }
+
+    /// Create `Revision` from `KeyRevision`
+    pub(crate) fn as_revision(&self) -> Revision {
+        Revision::new(self.mod_revision, self.sub_revision)
+    }
+}
