902e12ef97eae66b9ce3a93b5e3b54cfff5eb378
Jicheng Shi <jicheng.shi@datenlord.io>
2022-07-06 00:51:16 +0800

    optimize keybased channel
    1. record count for proceeder.
    2. make "key message" clone and equal based on the Arc.
    3. buf messages on the receiver side.

--- curp/src/keybased_channel.rs
+++ curp/src/keybased_channel.rs
@@ -4,6 +4,7 @@ use std::hash::Hash;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 
+use clippy_utilities::OverflowArithmetic;
 use event_listener::{Event, EventListener};
 use itertools::Itertools;
 use parking_lot::Mutex;
@@ -12,13 +13,48 @@ use tokio::task::JoinHandle;
 
 use crate::cmd::ConflictCheck;
 
-/// Keys and Messages combined structure
-#[derive(Hash, PartialEq, Eq)]
-pub(crate) struct KeysMessage<K, M> {
+/// The inner type for Keys and Messages combined structure
+struct KeysMessageInner<K, M> {
     /// The keys
     keys: Vec<K>,
     /// The message
     message: M,
+}
+
+impl<K, M> KeysMessageInner<K, M> {
+    /// Create a new `KeysMessageInner` structure
+    fn new(keys: Vec<K>, message: M) -> Self {
+        Self { keys, message }
+    }
+}
+
+/// Keys and Messages combined structure
+pub(crate) struct KeysMessage<K, M> {
+    /// Inner
+    inner: Arc<KeysMessageInner<K, M>>,
+}
+
+impl<K, M> Hash for KeysMessage<K, M> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Arc::<KeysMessageInner<K, M>>::as_ptr(&self.inner).hash(state);
+    }
+}
+
+impl<K, M> PartialEq for KeysMessage<K, M> {
+    fn eq(&self, other: &Self) -> bool {
+        Arc::<KeysMessageInner<K, M>>::as_ptr(&self.inner)
+            == Arc::<KeysMessageInner<K, M>>::as_ptr(&other.inner)
+    }
+}
+
+impl<K, M> Eq for KeysMessage<K, M> {}
+
+impl<K, M> Clone for KeysMessage<K, M> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::<_>::clone(&self.inner),
+        }
+    }
 }
 
 #[allow(unsafe_code)]
@@ -27,40 +63,40 @@ unsafe impl<K: Send, M: Send> Send for KeysMessage<K, M> {}
 impl<K, M> KeysMessage<K, M> {
     /// Create a new `KeysMessage`
     fn new(keys: Vec<K>, message: M) -> Self {
-        Self { keys, message }
+        Self {
+            inner: Arc::new(KeysMessageInner::new(keys, message)),
+        }
     }
 
     /// Get keys
     fn keys(&self) -> &[K] {
-        self.keys.as_ref()
+        self.inner.keys.as_ref()
     }
 
     /// Get the message
     #[allow(dead_code)]
     fn message(&self) -> &M {
-        &self.message
+        &self.inner.message
     }
 }
 
-/// The `HashSet` for holding the proceeders
-type Proceeders<K, M> = HashSet<Arc<KeysMessage<K, M>>>;
 /// The `HashSet` for holding the successors
-type Successors<K, M> = HashSet<Arc<KeysMessage<K, M>>>;
+type Successors<K, M> = HashSet<KeysMessage<K, M>>;
 
 /// This is a kind of key-based channel. It receives and sends messages as usual, but the recv end can't receive messages for the same key simutanously.
 /// Any message received comes with a notifier, and dropping the notifier breaks the blocking barrier for the key. When the notifier is holded, any message
 /// with the same key is blocked.
-struct KeybasedChannel<K: Eq + Hash + ConflictCheck, M: Eq + Hash> {
-    /// Proceeders that arrive eailer and have key confliction with this message
-    proceeder: HashMap<Arc<KeysMessage<K, M>>, Proceeders<K, M>>,
+struct KeybasedChannel<K: Eq + Hash + ConflictCheck, M> {
+    /// Proceeders that arrive eailer and have key confliction with this message, we only record count
+    proceeder: HashMap<KeysMessage<K, M>, u64>,
     /// Successors that arrive later and have key confliction with this message
-    successor: HashMap<Arc<KeysMessage<K, M>>, Successors<K, M>>,
+    successor: HashMap<KeysMessage<K, M>, Successors<K, M>>,
     /// The real queue the receivers get the message from.
-    inner: VecDeque<Arc<KeysMessage<K, M>>>,
+    inner: VecDeque<KeysMessage<K, M>>,
     /// The notifier notifies the new arrived message in the `inner`
     new_msg_event: Event,
     /// The message complete sender. Once a message is complete, the corresponding key is sent via this sender to unblock following same-key messages.
-    msg_complete_tx: UnboundedSender<Arc<KeysMessage<K, M>>>,
+    msg_complete_tx: UnboundedSender<KeysMessage<K, M>>,
 }
 
 /// If two key sets conflict with each other
@@ -70,79 +106,87 @@ fn keys_conflict<K: ConflictCheck>(ks1: &[K], ks2: &[K]) -> bool {
         .any(|(k1, k2)| k1.is_conflict(k2))
 }
 
-impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannel<K, M> {
+impl<K: Eq + Hash + Clone + ConflictCheck, M> KeybasedChannel<K, M> {
     /// Insert successors and proceeders info and return if it conflicts with any previous message
-    fn insert_conflict(&mut self, new_km: Arc<KeysMessage<K, M>>) -> bool {
-        let proceders: HashSet<_> = self
+    fn insert_conflict(&mut self, new_km: KeysMessage<K, M>) -> bool {
+        let proceeder_cnt: u64 = self
             .successor
             .iter_mut()
             .filter_map(|(k, v)| {
                 keys_conflict(k.keys(), new_km.keys()).then(|| {
-                    let _ignore = v.insert(Arc::<_>::clone(&new_km));
-                    Arc::<_>::clone(k)
+                    let _ignore = v.insert(new_km.clone());
+                    1
                 })
             })
-            .collect();
-        let is_conflict = !proceders.is_empty();
-        let _ignore = self
-            .successor
-            .insert(Arc::<_>::clone(&new_km), HashSet::new());
+            .sum();
+        let is_conflict = proceeder_cnt != 0;
+        let _ignore = self.successor.insert(new_km.clone(), HashSet::new());
 
         if is_conflict {
-            let _ignore2 = self.proceeder.insert(new_km, proceders);
+            let _ignore2 = self.proceeder.insert(new_km, proceeder_cnt);
         }
         is_conflict
     }
 
     /// Append a key and message to this `KeybasedChannel`
     fn append(&mut self, keys: &[K], msg: M) {
-        let km = Arc::new(KeysMessage::new(keys.to_vec(), msg));
-        if !self.insert_conflict(Arc::<_>::clone(&km)) {
+        let km = KeysMessage::new(keys.to_vec(), msg);
+        if !self.insert_conflict(km.clone()) {
             self.inner.push_back(km);
             self.new_msg_event.notify(1);
         }
     }
 
     /// Move a message for the `key` from pending to inner
-    fn mark_complete(&mut self, km: &Arc<KeysMessage<K, M>>) {
+    fn mark_complete(&mut self, km: &KeysMessage<K, M>) {
         if let Some(successors) = self.successor.get(km) {
-            for s in successors.iter() {
-                let is_ready = if let Some(proceeder) = self.proceeder.get_mut(s) {
-                    let _ignore = proceeder.remove(km);
-                    proceeder.is_empty()
-                } else {
-                    false
-                };
+            let notify_cnt: usize = successors
+                .iter()
+                .map(|s| {
+                    let is_ready = if let Some(proceeder) = self.proceeder.get_mut(s) {
+                        *proceeder = proceeder.overflow_sub(1);
+                        *proceeder == 0
+                    } else {
+                        false
+                    };
 
-                if is_ready {
-                    let _ignore = self.proceeder.remove(s);
-                    self.inner.push_back(Arc::<_>::clone(s));
-                    self.new_msg_event.notify(1);
-                }
+                    if is_ready {
+                        let _ignore = self.proceeder.remove(s);
+                        self.inner.push_back(s.clone());
+                        1
+                        // self.new_msg_event.notify(1);
+                    } else {
+                        0
+                    }
+                })
+                .sum();
+
+            if notify_cnt > 0 {
+                self.new_msg_event.notify(notify_cnt);
             }
         }
 
         let _ignore = self.successor.remove(km);
     }
 
-    /// Try to receive a message from this Queue.
-    /// Return `None` if there's nothing in the `inner`, otherwise
-    /// return `Some(message)`
-    fn try_recv(&mut self) -> Option<Arc<KeysMessage<K, M>>> {
-        self.inner.pop_front()
+    /// Swap a new queue with the inner queue
+    fn swap_inner(&mut self, new_queue: &mut VecDeque<KeysMessage<K, M>>) {
+        assert!(new_queue.is_empty());
+        std::mem::swap(&mut self.inner, new_queue);
     }
 }
 
+#[derive(Clone)]
 /// The Sender for the `KeybasedChannel`
-pub(crate) struct KeybasedChannelSender<K: Eq + Hash + ConflictCheck, M: Eq + Hash> {
+pub(crate) struct KeybasedChannelSender<K: Eq + Hash + ConflictCheck, M> {
     /// The channel
     channel: Arc<Mutex<KeybasedChannel<K, M>>>,
 }
 
-impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelSender<K, M> {
+impl<K: Eq + Hash + Clone + ConflictCheck, M> KeybasedChannelSender<K, M> {
     /// Send a key and message to the channel
     #[allow(dead_code)]
-    pub(crate) fn send(&self, keys: &[K], msg: M) -> UnboundedSender<Arc<KeysMessage<K, M>>> {
+    pub(crate) fn send(&self, keys: &[K], msg: M) -> UnboundedSender<KeysMessage<K, M>> {
         let mut channel = self.channel.lock();
         channel.append(keys, msg);
         channel.msg_complete_tx.clone()
@@ -150,14 +194,16 @@ impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelSender<K
 }
 
 /// The Receiver for the `KeybasedChannel`
-pub(crate) struct KeybasedChannelReceiver<K: Eq + Hash + ConflictCheck, M: Eq + Hash> {
+pub(crate) struct KeybasedChannelReceiver<K: Eq + Hash + ConflictCheck, M> {
     /// The channel
     channel: Arc<Mutex<KeybasedChannel<K, M>>>,
+    /// Local message buf
+    buf: VecDeque<KeysMessage<K, M>>,
     /// The join handler for the task, which handles the message complete event.
     handle: JoinHandle<()>,
 }
 
-impl<K: Eq + Hash + ConflictCheck, M: Eq + Hash> Drop for KeybasedChannelReceiver<K, M> {
+impl<K: Eq + Hash + ConflictCheck, M> Drop for KeybasedChannelReceiver<K, M> {
     fn drop(&mut self) {
         self.handle.abort();
     }
@@ -171,31 +217,26 @@ enum MessageOrListener<M> {
     Listen(EventListener),
 }
 
-impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelReceiver<K, M> {
+impl<K: Eq + Hash + Clone + ConflictCheck, M> KeybasedChannelReceiver<K, M> {
     /// Get a message from the channel or a waiting listener
     #[allow(clippy::expect_used, clippy::unwrap_in_result)]
-    fn get_msg_or_listener(&self) -> MessageOrListener<Arc<KeysMessage<K, M>>> {
-        let mut channel = self.channel.lock();
-        let mut local_listener: Option<EventListener> = None;
-        loop {
-            match channel.try_recv() {
-                Some(msg) => return MessageOrListener::Message(msg),
-                #[allow(clippy::match_ref_pats)] // don't move listener out
-                None => match &local_listener {
-                    &Some(_) => {
-                        break;
-                    }
-                    &None => local_listener = Some(channel.new_msg_event.listen()),
-                },
-            }
+    fn get_msg_or_listener(&mut self) -> MessageOrListener<KeysMessage<K, M>> {
+        if let Some(buf_msg) = self.buf.pop_front() {
+            return MessageOrListener::Message(buf_msg);
         }
-        MessageOrListener::Listen(local_listener.expect("local_listener should not be none"))
+
+        let mut channel = self.channel.lock();
+        channel.swap_inner(&mut self.buf);
+        match self.buf.pop_front() {
+            Some(msg) => MessageOrListener::Message(msg),
+            None => MessageOrListener::Listen(channel.new_msg_event.listen()),
+        }
     }
 
     /// Receive a message
     /// Return (message, `msg_complete_sender`)
     #[allow(dead_code)]
-    pub(crate) fn recv(&self) -> Arc<KeysMessage<K, M>> {
+    pub(crate) fn recv(&mut self) -> KeysMessage<K, M> {
         loop {
             match self.get_msg_or_listener() {
                 MessageOrListener::Message(msg) => return msg,
@@ -208,7 +249,7 @@ impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelReceiver
     /// Return None if `timeout` hits,
     /// otherwise return Some((message, `msg_complete_sender`))
     #[allow(dead_code, clippy::expect_used, clippy::unwrap_in_result)]
-    pub(crate) fn recv_timeout(&self, mut timeout: Duration) -> Option<Arc<KeysMessage<K, M>>> {
+    pub(crate) fn recv_timeout(&mut self, mut timeout: Duration) -> Option<KeysMessage<K, M>> {
         let mut start = SystemTime::now();
         loop {
             match self.get_msg_or_listener() {
@@ -234,7 +275,7 @@ impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelReceiver
 
     /// Receive a message async.
     #[allow(dead_code)]
-    pub(crate) async fn async_recv(&self) -> Arc<KeysMessage<K, M>> {
+    pub(crate) async fn async_recv(&mut self) -> KeysMessage<K, M> {
         loop {
             match self.get_msg_or_listener() {
                 MessageOrListener::Message(msg) => return msg,
@@ -249,7 +290,7 @@ impl<K: Eq + Hash + Clone + ConflictCheck, M: Eq + Hash> KeybasedChannelReceiver
 #[allow(dead_code)]
 pub(crate) fn channel<
     K: Clone + Eq + Hash + Send + Sync + ConflictCheck + 'static,
-    M: Eq + Hash + Send + Sync + 'static,
+    M: Send + 'static,
 >() -> (KeybasedChannelSender<K, M>, KeybasedChannelReceiver<K, M>) {
     let (tx, mut rx) = unbounded_channel();
 
@@ -275,6 +316,7 @@ pub(crate) fn channel<
         },
         KeybasedChannelReceiver {
             channel: inner_channel,
+            buf: VecDeque::new(),
             handle,
         },
     )
@@ -289,7 +331,7 @@ mod test {
     #[allow(clippy::unwrap_used, unused_results)]
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_channel() {
-        let (tx, rx) = channel::<String, String>();
+        let (tx, mut rx) = channel::<String, String>();
 
         let complete = tx.send(&["1".to_owned()], "A".to_owned());
         tx.send(&["1".to_owned()], "B".to_owned());
@@ -311,7 +353,7 @@ mod test {
     #[allow(clippy::unwrap_used, unused_results)]
     #[tokio::test(flavor = "multi_thread", worker_threads = 3)]
     async fn test_channel_async() {
-        let (tx, rx) = channel::<String, String>();
+        let (tx, mut rx) = channel::<String, String>();
 
         let complete = tx.send(&["1".to_owned()], "A".to_owned());
         tx.send(&["1".to_owned()], "B".to_owned());
