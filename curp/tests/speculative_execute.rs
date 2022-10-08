9789a673ef7ac3ec9b773f756bb51894f0926333
Jicheng Shi <jicheng.shi@datenlord.com>
2022-10-08 17:51:30 +0800

    add test to verify replica cmd execution and after_sync call

--- curp/tests/speculative_execute.rs
+++ curp/tests/speculative_execute.rs
@@ -6,7 +6,7 @@ mod common;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 10)]
 async fn speculative_execute() {
-    let (mut rx, client) = create_servers_client().await;
+    let (mut exe_rx, mut after_sync_rx, client) = create_servers_client().await;
     let result = client
         .propose(TestCommand::new(
             ProposeId::new("id1".to_owned()),
@@ -19,7 +19,15 @@ async fn speculative_execute() {
     assert!(result.is_ok());
     assert_eq!(result.unwrap(), TestCommandResult::GetResult("".to_owned()));
 
-    let (t, key) = rx.recv().await.unwrap();
-    assert_eq!(t, TestCommandType::Get);
-    assert_eq!(key, "A".to_owned());
+    for _ in 0..3 {
+        let (t, key) = exe_rx.recv().await.unwrap();
+        assert_eq!(t, TestCommandType::Get);
+        assert_eq!(key, "A".to_owned());
+    }
+
+    for _ in 0..3 {
+        let (t, key) = after_sync_rx.recv().await.unwrap();
+        assert_eq!(t, TestCommandType::Get);
+        assert_eq!(key, "A".to_owned());
+    }
 }
