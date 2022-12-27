bc484861246f3261a53c69026b91aab827f9e6bb
Phoeniix Zhao <Phoenix500526@163.com>
2022-12-27 20:24:20 +0800

    adding a protoc version requirement in quick start
    fix issue:[Xline]Update protoc version information in quick start #118
    
    Signed-off-by: Phoeniix Zhao <Phoenix500526@163.com>

--- curp/build.rs
+++ curp/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["./proto/message.proto"], &["./proto/"])
         .unwrap_or_else(|e| panic!("Failed to compile proto, error is {:?}", e));
 }
