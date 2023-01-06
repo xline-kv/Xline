f87bf974afff2ddf528283534ec07046ca31bbe7
markcty <markcty@outlook.com>
2023-01-06 15:45:11 +0800

    fix coverage scripts warnings, exclude coverage for tests, and change cov type

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 apt-get install -y cmake g++
 
 # install protoc
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,47 +1,57 @@
+#/bin/bash
 rm -rf coverage
 mkdir coverage
 
-if [[ -z "${CI}" ]]; then
+if [ -z "${CI}" ]; then
   FORMAT=html
   OUTPUT_XLINE=coverage/xline_html
   OUTPUT_CURP=coverage/curp_html
   OUTPUT_UTILS=coverage/utils_html
 else
-  FORMAT=cobertura
-  OUTPUT_XLINE=coverage/xline_cov.xml
-  OUTPUT_CURP=coverage/curp_cov.xml
-  OUTPUT_UTILS=coverage/utils_cov.xml
+  FORMAT=lcov
+  OUTPUT_XLINE=coverage/xline_cov.lcovrc
+  OUTPUT_CURP=coverage/curp_cov.lcovrc
+  OUTPUT_UTILS=coverage/utils_cov.lcovrc
 fi
 
 # generate coverage data
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='coverage-%p-%m.profraw' cargo test
 
 # generate report
-echo "generating xline coverage"
+echo "generating xline coverage..."
 grcov . \
   --binary-path ./target/debug/ \
   --source-dir ./xline/src \
   -t $FORMAT \
   --branch \
   --ignore-not-existing \
+  --excl-br-start '^(pub(\((crate|super)\))? )?mod tests' \
+  --excl-br-stop '^}' \
+  --ignore="*/tests/*" \
   -o $OUTPUT_XLINE
 
-echo "generating curp coverage"
+echo "generating curp coverage..."
 grcov . \
   --binary-path ./target/debug/ \
   --source-dir ./curp/src \
   -t $FORMAT \
   --branch \
   --ignore-not-existing \
+  --excl-br-start '^(pub(\((crate|super)\))? )?mod tests' \
+  --excl-br-stop '^}' \
+  --ignore="*/tests/*" \
   -o $OUTPUT_CURP
 
-echo "generating utils coverage"
+echo "generating utils coverage..."
 grcov . \
   --binary-path ./target/debug/ \
   --source-dir ./utils/src \
   -t $FORMAT \
   --branch \
   --ignore-not-existing \
+  --excl-br-start '^(pub(\((crate|super)\))? )?mod tests' \
+  --excl-br-stop '^}' \
+  --ignore="*/tests/*" \
   -o $OUTPUT_UTILS
 
 # cleanup
