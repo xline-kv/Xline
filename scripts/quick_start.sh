356e307289442f618cd0bee2bc8c082de4513c9d
themanforfree <themanforfree@gmail.com>
2023-01-19 12:10:48 +0800

    Execute etcd validation test automatically
    close #151

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-WORKDIR=$(pwd)
+DIR=$(
+    cd "$(dirname "$0")"
+    pwd
+)
 SERVERS=("172.20.0.2" "172.20.0.3" "172.20.0.4" "172.20.0.5")
 MEMBERS="node1=${SERVERS[1]}:2379,node2=${SERVERS[2]}:2379,node3=${SERVERS[3]}:2379"
 
@@ -9,7 +12,9 @@ MEMBERS="node1=${SERVERS[1]}:2379,node2=${SERVERS[2]}:2379,node3=${SERVERS[3]}:2
 run_xline() {
     cmd="/usr/local/bin/xline \
     --name node${1} \
-    --members ${MEMBERS}"
+    --members ${MEMBERS} \
+    --auth-public-key /mnt/public.pem \
+    --auth-private-key /mnt/private.pem"
 
     if [ ${1} -eq 1 ]; then
         cmd="${cmd} --is-leader"
@@ -45,9 +50,9 @@ run_container() {
     size=${1}
     image="datenlord/xline:latest"
     for ((i = 1; i <= ${size}; i++)); do
-        docker run -d -it --rm --name=node${i} --net=xline_net --ip=172.20.0.$((i + 2)) --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${WORKDIR}:/mnt ${image} bash &
+        docker run -d -it --rm --name=node${i} --net=xline_net --ip=172.20.0.$((i + 2)) --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ${image} bash &
     done
-    docker run -d -it --rm --name=node4 --net=xline_net --ip=172.20.0.2 --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${WORKDIR}:/mnt gcr.io/etcd-development/etcd:v3.5.5 bash &
+    docker run -d -it --rm --name=node4 --net=xline_net --ip=172.20.0.2 --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${DIR}:/mnt gcr.io/etcd-development/etcd:v3.5.5 bash &
     wait
     echo container started
 }
