20fedccd4471fbf153e9c887763526beaaca79fb
bsbds <69835502+bsbds@users.noreply.github.com>
2023-06-05 15:10:11 +0800

    feat: implement maintenance client for xline-client
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xline-client/src/clients/maintenance.rs
+++ xline-client/src/clients/maintenance.rs
@@ -1,11 +1,12 @@
-// TODO: Remove these when the placeholder is implemented.
-#![allow(dead_code)]
-
 use std::{fmt::Debug, sync::Arc};
 
-use tonic::transport::Channel;
+use tonic::{transport::Channel, Streaming};
+use xlineapi::{self, SnapshotRequest, SnapshotResponse};
 
-use crate::AuthService;
+use crate::{
+    error::{ClientError, Result},
+    AuthService,
+};
 
 /// The maintenance client
 #[derive(Clone, Debug)]
@@ -25,5 +26,20 @@ impl MaintenanceClient {
                 token.and_then(|t| t.parse().ok().map(Arc::new)),
             )),
         }
+    }
+
+    /// Gets a snapshot over a stream
+    ///
+    /// # Errors
+    ///
+    /// If the RPC client fails to send request
+    #[inline]
+    pub async fn snapshot(&mut self) -> Result<Streaming<SnapshotResponse>> {
+        Ok(self
+            .inner
+            .snapshot(SnapshotRequest {})
+            .await
+            .map_err(Into::<ClientError>::into)?
+            .into_inner())
     }
 }
