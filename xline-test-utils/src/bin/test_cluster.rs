c60a67d2a9e849f1389041dd6f361d745c639b46
bsbds <69835502+bsbds@users.noreply.github.com>
2023-06-13 19:54:10 +0800

    test: add a test cluster binary in `xline-test-utils`

---
+++ xline-test-utils/src/bin/test_cluster.rs
@@ -0,0 +1,19 @@
+//! The binary is to quickly setup a cluster for convenient testing
+use tokio::signal;
+use xline_test_utils::Cluster;
+
+#[tokio::main]
+async fn main() {
+    let mut cluster = Cluster::new(3).await;
+    cluster.start().await;
+
+    println!("cluster running");
+
+    for (id, addr) in cluster.addrs() {
+        println!("server id: {} addr: {}", id, addr);
+    }
+
+    if let Err(e) = signal::ctrl_c().await {
+        eprintln!("Unable to listen for shutdown signal: {e}");
+    }
+}
