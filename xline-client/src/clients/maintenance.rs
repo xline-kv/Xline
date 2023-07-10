183ea8f26942b5978b6e2ceee9e440638268f307
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-10 15:21:25 +0800

    docs: add examples to maintenance client methods
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xline-client/src/clients/maintenance.rs
+++ xline-client/src/clients/maintenance.rs
@@ -33,6 +33,41 @@ impl MaintenanceClient {
     /// # Errors
     ///
     /// This function will return an error if the inner RPC client encountered a propose failure
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use xline_client::{error::Result, Client, ClientOptions};
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> Result<()> {
+    ///     // the name and address of all curp members
+    ///     let curp_members = [
+    ///         ("server0", "10.0.0.1:2379"),
+    ///         ("server1", "10.0.0.2:2379"),
+    ///         ("server2", "10.0.0.3:2379"),
+    ///     ];
+    ///
+    ///     let mut client = Client::connect(curp_members, ClientOptions::default())
+    ///         .await?
+    ///         .maintenance_client();
+    ///
+    ///     // snapshot
+    ///     let mut msg = client.snapshot().await?;
+    ///     let mut snapshot = vec![];
+    ///     loop {
+    ///         if let Some(resp) = msg.message().await? {
+    ///             snapshot.extend_from_slice(&resp.blob);
+    ///             if resp.remaining_bytes == 0 {
+    ///                 break;
+    ///             }
+    ///         }
+    ///     }
+    ///     println!("snapshot size: {}", snapshot.len());
+    ///
+    ///     Ok(())
+    /// }
+    /// ```
     #[inline]
     pub async fn snapshot(&mut self) -> Result<Streaming<SnapshotResponse>> {
         Ok(self
