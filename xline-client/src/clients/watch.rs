cf0ce44229606b3754892469ef1416e3badc25ad
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-10 15:18:34 +0800

    docs: add examples to watch client methods
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xline-client/src/clients/watch.rs
+++ xline-client/src/clients/watch.rs
@@ -45,6 +45,46 @@ impl WatchClient {
     /// # Panics
     ///
     /// This function will panic if the RPC server doesn't return a create watch response
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use xline_client::{
+    ///     error::Result,
+    ///     types::{kv::PutRequest, watch::WatchRequest},
+    ///     Client, ClientOptions,
+    /// };
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> Result<()> {
+    ///     let curp_members = [
+    ///         ("server0", "10.0.0.1:2379"),
+    ///         ("server1", "10.0.0.2:2379"),
+    ///         ("server2", "10.0.0.3:2379"),
+    ///     ];
+    ///
+    ///     let client = Client::connect(curp_members, ClientOptions::default()).await?;
+    ///     let mut watch_client = client.watch_client();
+    ///     let mut kv_client = client.kv_client();
+    ///
+    ///     let (mut watcher, mut stream) = watch_client.watch(WatchRequest::new("key1")).await?;
+    ///     kv_client.put(PutRequest::new("key1", "value1")).await?;
+    ///
+    ///     let resp = stream.message().await?.unwrap();
+    ///     let kv = resp.events[0].kv.as_ref().unwrap();
+    ///
+    ///     println!(
+    ///         "got key: {}, value: {}",
+    ///         String::from_utf8_lossy(&kv.key),
+    ///         String::from_utf8_lossy(&kv.value)
+    ///     );
+    ///
+    ///     // cancel the watch
+    ///     watcher.cancel()?;
+    ///
+    ///     Ok(())
+    /// }
+    /// ```
     #[inline]
     pub async fn watch(
         &mut self,
