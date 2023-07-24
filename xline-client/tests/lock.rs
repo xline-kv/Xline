e9f23e2cc25578acaf7088116e5d17767f381baf
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-24 10:54:22 +0800

    test: add more tests to lock client
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xline-client/tests/lock.rs
+++ xline-client/tests/lock.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use common::get_cluster_client;
+use test_macros::abort_on_panic;
 use xline_client::{
     error::Result,
     types::lock::{LockRequest, UnlockRequest},
@@ -14,10 +17,67 @@ async fn lock_unlock_should_success_in_normal_path() -> Result<()> {
     let resp = client
         .lock(LockRequest::new().with_name("lock-test"))
         .await?;
-    let key = resp.key;
-    let key_str = std::str::from_utf8(&key).unwrap();
-    assert!(key_str.starts_with("lock-test/"));
+    assert!(resp.key.starts_with(b"lock-test/"));
 
-    client.unlock(UnlockRequest::new().with_key(key)).await?;
+    client
+        .unlock(UnlockRequest::new().with_key(resp.key))
+        .await?;
+    Ok(())
+}
+
+#[tokio::test]
+#[abort_on_panic]
+async fn lock_contention_should_occur_when_acquire_by_two() -> Result<()> {
+    let (_cluster, client) = get_cluster_client().await?;
+    let mut client = client.lock_client();
+    let mut client_c = client.clone();
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+        let resp = client_c
+            .lock(LockRequest::new().with_name("lock-test"))
+            .await
+            .unwrap();
+        let _ignore = tx.send(());
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        let _resp = client_c
+            .unlock(UnlockRequest::new().with_key(resp.key))
+            .await
+            .unwrap();
+    });
+
+    rx.recv().await.unwrap();
+    let now = tokio::time::Instant::now();
+    let resp = client
+        .lock(LockRequest::new().with_name("lock-test"))
+        .await
+        .unwrap();
+
+    assert!(now.elapsed() > Duration::from_secs(1));
+    assert!(resp.key.starts_with(b"lock-test/"));
+
+    Ok(())
+}
+
+#[tokio::test]
+#[abort_on_panic]
+async fn lock_should_timeout_when_ttl_is_set() -> Result<()> {
+    let (_cluster, client) = get_cluster_client().await?;
+    let mut client = client.lock_client();
+
+    let _resp = client
+        .lock(LockRequest::new().with_name("lock-test").with_ttl(1))
+        .await
+        .unwrap();
+
+    let resp = tokio::time::timeout(
+        Duration::from_secs(2),
+        client.lock(LockRequest::new().with_name("lock-test")),
+    )
+    .await
+    .expect("timeout when trying to lock")?;
+
+    assert!(resp.key.starts_with(b"lock-test/"));
+
     Ok(())
 }
