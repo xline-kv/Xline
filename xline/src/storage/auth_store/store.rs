1a13c14255790ab7056dac1024b3a31884e3752e
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-25 19:01:22 +0800

    refactor: move revsion check back to `kv_server`
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xline/src/storage/auth_store/store.rs
+++ xline/src/storage/auth_store/store.rs
@@ -296,7 +296,7 @@ where
         req: &AuthUserAddRequest,
     ) -> Result<AuthUserAddResponse, ExecuteError> {
         debug!("handle_user_add_request");
-        req.validation(())?;
+        req.validation()?;
         if self.backend.get_user(&req.name).is_ok() {
             return Err(ExecuteError::UserAlreadyExists(req.name.clone()));
         }
@@ -406,7 +406,7 @@ where
         req: &AuthRoleAddRequest,
     ) -> Result<AuthRoleAddResponse, ExecuteError> {
         debug!("handle_role_add_request");
-        req.validation(())?;
+        req.validation()?;
         if self.backend.get_role(&req.name).is_ok() {
             return Err(ExecuteError::RoleAlreadyExists(req.name.clone()));
         }
@@ -477,7 +477,7 @@ where
         req: &AuthRoleGrantPermissionRequest,
     ) -> Result<AuthRoleGrantPermissionResponse, ExecuteError> {
         debug!("handle_role_grant_permission_request");
-        req.validation(())?;
+        req.validation()?;
         let _role = self.backend.get_role(&req.name)?;
         Ok(AuthRoleGrantPermissionResponse {
             header: Some(self.header_gen.gen_auth_header()),
