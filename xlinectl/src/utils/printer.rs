b15504b0e9460089fbc171661ca01163806fb4bc
bsbds <69835502+bsbds@users.noreply.github.com>
2023-07-27 19:46:38 +0800

    refactor: add a new trait that control the printer type in xlinectl
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- xlinectl/src/utils/printer.rs
+++ xlinectl/src/utils/printer.rs
@@ -1,11 +1,88 @@
-use xlineapi::{KeyValue, ResponseHeader};
+use std::sync::OnceLock;
 
-/// Printer of common response types
-pub(crate) struct Printer;
+use xlineapi::{KeyValue, PutResponse, RangeResponse, ResponseHeader};
 
-impl Printer {
+/// The global printer type config
+static PRINTER_TYPE: OnceLock<PrinterType> = OnceLock::new();
+
+/// The type of the Printer
+pub(crate) enum PrinterType {
+    /// Simple printer, which print simplified result
+    Simple,
+    /// Filed printer, which print every fields of the result
+    Field,
+}
+
+/// Set the type of the printer
+pub(crate) fn set_printer_type(printer_type: PrinterType) {
+    let _ignore = PRINTER_TYPE.get_or_init(|| printer_type);
+}
+
+/// The printer implementation trait
+pub(crate) trait Printer {
+    /// Print the simplified result
+    fn simple(&self);
+    /// Print every fields of the result
+    fn field(&self);
+    /// Print according to the config set
+    fn print(&self) {
+        match *PRINTER_TYPE
+            .get()
+            .unwrap_or_else(|| unreachable!("the printer type should be initialized"))
+        {
+            PrinterType::Simple => self.simple(),
+            PrinterType::Field => self.field(),
+        }
+    }
+}
+
+impl Printer for PutResponse {
+    fn simple(&self) {
+        println!("OK");
+    }
+
+    fn field(&self) {
+        FieldPrinter::header(self.header.as_ref());
+        if let Some(pre_kv) = self.prev_kv.as_ref() {
+            FieldPrinter::kv(pre_kv);
+        }
+    }
+}
+
+impl Printer for RangeResponse {
+    fn simple(&self) {
+        for kv in &self.kvs {
+            SimplePrinter::utf8(&kv.key);
+            SimplePrinter::utf8(&kv.value);
+        }
+    }
+
+    fn field(&self) {
+        FieldPrinter::header(self.header.as_ref());
+        println!("kvs:");
+        for kv in &self.kvs {
+            FieldPrinter::kv(kv);
+        }
+        println!("more: {}, count: {}", self.more, self.count);
+    }
+}
+
+/// Simple Printer of common response types
+struct SimplePrinter;
+
+impl SimplePrinter {
+    /// Print utf8 bytes as string
+    fn utf8(vec: &[u8]) {
+        println!("{}", String::from_utf8_lossy(vec));
+    }
+}
+
+/// Field Printer of common response types
+struct FieldPrinter;
+
+impl FieldPrinter {
     /// Response header printer
-    pub(crate) fn header(header: Option<&ResponseHeader>) {
+    fn header(header: Option<&ResponseHeader>) {
         let Some(header) = header else { return };
         println!("header:");
         println!(
@@ -15,7 +92,7 @@ impl Printer {
     }
 
     /// Response key-value printer
-    pub(crate) fn kv(kv: &KeyValue) {
+    fn kv(kv: &KeyValue) {
         println!(
             "key: {}, value: {}",
             String::from_utf8_lossy(&kv.key),
