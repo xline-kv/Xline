ecdc88d171773b03e446f2ee8502e692df266702
Lan <gcslyp@gmail.com>
2023-07-28 13:08:05 +0800

    add e2e ci for kubernetes with kind
    Signed-off-by: Lan Liang <gcslyp@gmail.com>

---
+++ .github/scripts/kind.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+wget -q https://github.com/kubernetes-sigs/kind/releases/download/v0.20.0/kind-linux-amd64
+chmod +x kind-linux-amd64 && mv kind-linux-amd64 /usr/local/bin/kind
+
+K8SVERSION=${K8SVERSION:-"v1.25.3"}
+WORKSPACE=$PWD
+
+sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/.github/scripts/kind.yaml
+sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/.github/scripts/kind.yaml
+kind create cluster --config $WORKSPACE/.github/scripts/kind.yaml
+kubectl wait node --all --for condition=ready
