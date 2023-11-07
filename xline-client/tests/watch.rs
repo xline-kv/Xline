27055f367b52200d854077dfbad2790e05c864b5
iGxnon <igxnon@gmail.com>
2023-11-07 09:30:58 +0800

    test: add test for #505
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- xline-client/tests/watch.rs
+++ xline-client/tests/watch.rs
@@ -38,3 +38,25 @@ async fn watch_should_receive_consistent_events() -> Result<()> {
 
     Ok(())
 }
+
+/// To ensure #505 is fixed
+#[tokio::test(flavor = "multi_thread")]
+async fn watch_stream_should_work_after_watcher_dropped() -> Result<()> {
+    let (_cluster, client) = get_cluster_client().await.unwrap();
+    let mut watch_client = client.watch_client();
+    let kv_client = client.kv_client();
+
+    let (_, mut stream) = watch_client.watch(WatchRequest::new("watch01")).await?;
+
+    kv_client.put(PutRequest::new("watch01", "01")).await?;
+
+    let resp = stream.message().await?.unwrap();
+    assert_eq!(resp.events.len(), 1);
+
+    let kv = resp.events[0].kv.as_ref().unwrap();
+    assert_eq!(kv.key, b"watch01");
+    assert_eq!(kv.value, b"01");
+    assert_eq!(resp.events[0].r#type(), EventType::Put);
+
+    Ok(())
+}
