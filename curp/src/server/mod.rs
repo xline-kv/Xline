6f85fc4cbc107df0b8e3fb83b3f03591feec3a84
iGxnon <igxnon@gmail.com>
2023-11-09 21:57:14 +0800

    refactor: refactor curp error
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- curp/src/server/mod.rs
+++ curp/src/server/mod.rs
@@ -2,7 +2,6 @@ use std::{fmt::Debug, sync::Arc};
 
 use curp_external_api::cmd::{ConflictCheck, ProposeId};
 use engine::SnapshotAllocator;
-use futures::TryStreamExt;
 use serde::{Deserialize, Serialize};
 #[cfg(not(madsim))]
 use tokio::net::TcpListener;
@@ -14,7 +13,7 @@ use tracing::info;
 use tracing::instrument;
 use utils::{config::CurpConfig, shutdown, tracing::Extract};
 
-use self::curp_node::{CurpError, CurpNode};
+use self::curp_node::CurpNode;
 use crate::{
     cmd::{Command, CommandExecutor},
     error::ServerError,
@@ -153,19 +152,6 @@ impl<C: 'static + Command, RC: RoleChange + 'static> crate::rpc::InnerProtocol f
         ))
     }
 
-    #[instrument(skip_all, name = "curp_install_snapshot")]
-    async fn install_snapshot(
-        &self,
-        request: tonic::Request<tonic::Streaming<InstallSnapshotRequest>>,
-    ) -> Result<tonic::Response<InstallSnapshotResponse>, tonic::Status> {
-        let req_stream = request
-            .into_inner()
-            .map_err(|e| format!("snapshot transmission failed at client side, {e}"));
-        Ok(tonic::Response::new(
-            self.inner.install_snapshot(req_stream).await?,
-        ))
-    }
-
     #[instrument(skip_all, name = "curp_vote")]
     async fn vote(
         &self,
@@ -173,6 +159,17 @@ impl<C: 'static + Command, RC: RoleChange + 'static> crate::rpc::InnerProtocol f
     ) -> Result<tonic::Response<VoteResponse>, tonic::Status> {
         Ok(tonic::Response::new(
             self.inner.vote(request.into_inner()).await?,
+        ))
+    }
+
+    #[instrument(skip_all, name = "curp_install_snapshot")]
+    async fn install_snapshot(
+        &self,
+        request: tonic::Request<tonic::Streaming<InstallSnapshotRequest>>,
+    ) -> Result<tonic::Response<InstallSnapshotResponse>, tonic::Status> {
+        let req_stream = request.into_inner();
+        Ok(tonic::Response::new(
+            self.inner.install_snapshot(req_stream).await?,
         ))
     }
 }
@@ -206,7 +203,7 @@ impl<C: Command + 'static, RC: RoleChange + 'static> Rpc<C, RC> {
         {
             Ok(n) => n,
             Err(err) => {
-                panic!("failed to create curp service, {err}");
+                panic!("failed to create curp service, {err:?}");
             }
         };
 
