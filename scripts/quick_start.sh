1982d4b1186bc011cf05c8f761af77eddb4c72d6
Phoeniix Zhao <Phoenix500526@163.com>
2023-11-22 08:34:39 +0800

    build: modify the dockerfile and bootup logic
    Signed-off-by: Phoeniix Zhao <Phoenix500526@163.com>

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -6,36 +6,6 @@ DIR=$(
 SERVERS=("172.20.0.2" "172.20.0.3" "172.20.0.4" "172.20.0.5")
 MEMBERS="node1=${SERVERS[1]}:2379,${SERVERS[1]}:2380,node2=${SERVERS[2]}:2379,${SERVERS[2]}:2380,node3=${SERVERS[3]}:2379,${SERVERS[3]}:2380"
 
-# run xline node by index
-# args:
-#   $1: index of the node
-run_xline() {
-    cmd="/usr/local/bin/xline \
-    --name node${1} \
-    --members ${MEMBERS} \
-    --storage-engine rocksdb \
-    --data-dir /usr/local/xline/data-dir \
-    --auth-public-key /mnt/public.pem \
-    --auth-private-key /mnt/private.pem"
-
-    if [ ${1} -eq 1 ]; then
-        cmd="${cmd} --is-leader"
-    fi
-
-    docker exec -e RUST_LOG=debug -d node${1} ${cmd}
-    echo "command is: docker exec -e RUST_LOG=debug -d node${1} ${cmd}"
-}
-
-# run cluster of xline/etcd in container
-run_cluster() {
-    echo cluster starting
-    run_xline 1 &
-    run_xline 2 &
-    run_xline 3 &
-    wait
-    echo cluster started
-}
-
 # stop all containers
 stop_all() {
     echo stopping
@@ -57,9 +27,23 @@ run_container() {
     size=${1}
     image="ghcr.io/xline-kv/xline:latest"
     for ((i = 1; i <= ${size}; i++)); do
-        docker run -d -it --rm --name=node${i} --net=xline_net --ip=${SERVERS[$i]} --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ${image} bash &
+        if [ "$i" -eq 1 ]; then
+            docker run \
+            -e RUST_LOG=debug -e HOSTNAME=node${i} -e MEMBERS=${MEMBERS} -e IS_LEADER=true \
+            -d -it --rm --name=node${i} \
+            --net=xline_net --ip=${SERVERS[$i]} --cap-add=NET_ADMIN \
+            --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ${image} &
+        else
+            docker run \
+            -e RUST_LOG=debug -e HOSTNAME=node${i} -e MEMBERS=${MEMBERS} \
+            -d -it --rm --name=node${i} \
+            --net=xline_net --ip=${SERVERS[$i]} --cap-add=NET_ADMIN \
+            --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ${image} &
+        fi
     done
-    docker run -d -it --rm --name=client --net=xline_net --ip=${SERVERS[0]} --cap-add=NET_ADMIN --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ghcr.io/xline-kv/etcdctl:v3.5.9 bash &
+    docker run -d -it --rm  --name=client \
+        --net=xline_net --ip=${SERVERS[0]} --cap-add=NET_ADMIN \
+        --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ghcr.io/xline-kv/etcdctl:v3.5.9 bash &
     wait
     echo container started
 }
@@ -68,4 +52,3 @@ stop_all
 docker network create --subnet=172.20.0.0/24 xline_net >/dev/null 2>&1
 
 run_container 3
-run_cluster
