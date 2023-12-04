4ec29176f2381c76cbb397adb77df40a6af6326d
bsbds <69835502+bsbds@users.noreply.github.com>
2023-12-04 23:29:22 +0800

    feat: implement WAL segments
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- crates/curp/src/server/storage/wal/mod.rs
+++ crates/curp/src/server/storage/wal/mod.rs
@@ -9,5 +9,17 @@ mod error;
 /// File pipeline
 mod pipeline;
 
+/// WAL segment
+mod segment;
+
 /// File utils
 mod util;
+
+/// The magic of the WAL file
+const WAL_MAGIC: u32 = 0xd86e_0be2;
+
+/// The current WAL version
+const WAL_VERSION: u8 = 0x00;
+
+/// The wal file extension
+const WAL_FILE_EXT: &str = ".wal";
