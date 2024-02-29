2369dcb56ea13ffaf647971a422a99eec6d65efd
Phoeniix Zhao <Phoenix500526@163.com>
2024-02-29 16:31:45 +0800

    fix: add keep alive logic for xlinectl lock command
    Closes: #664
    Signed-off-by: Phoeniix Zhao <Phoenix500526@163.com>

--- crates/xline/tests/it/lock_test.rs
+++ crates/xline/tests/it/lock_test.rs
@@ -5,7 +5,7 @@ use tokio::time::{self, timeout};
 use xline_test_utils::{
     types::{
         lease::LeaseGrantRequest,
-        lock::{LockRequest, UnlockRequest},
+        lock::{LockRequest, UnlockRequest, DEFAULT_SESSION_TTL},
     },
     Cluster,
 };
@@ -17,11 +17,19 @@ async fn test_lock() -> Result<(), Box<dyn Error>> {
     cluster.start().await;
     let client = cluster.client().await;
     let lock_client = client.lock_client();
+    let lease_id_1 = client
+        .lease_client()
+        .grant(LeaseGrantRequest::new(DEFAULT_SESSION_TTL))
+        .await?
+        .id;
 
     let lock_handle = tokio::spawn({
         let c = lock_client.clone();
         async move {
-            let res = c.lock(LockRequest::new("test")).await.unwrap();
+            let res = c
+                .lock(LockRequest::new("test").with_lease(lease_id_1))
+                .await
+                .unwrap();
             time::sleep(Duration::from_secs(3)).await;
             let _res = c.unlock(UnlockRequest::new(res.key)).await.unwrap();
         }
@@ -29,7 +37,14 @@ async fn test_lock() -> Result<(), Box<dyn Error>> {
 
     time::sleep(Duration::from_secs(1)).await;
     let now = time::Instant::now();
-    let res = lock_client.lock(LockRequest::new("test")).await?;
+    let lease_id_2 = client
+        .lease_client()
+        .grant(LeaseGrantRequest::new(DEFAULT_SESSION_TTL))
+        .await?
+        .id;
+    let res = lock_client
+        .lock(LockRequest::new("test").with_lease(lease_id_2))
+        .await?;
     let elapsed = now.elapsed();
     assert!(res.key.starts_with(b"test"));
     assert!(elapsed >= Duration::from_secs(1));
@@ -46,18 +61,23 @@ async fn test_lock_timeout() -> Result<(), Box<dyn Error>> {
     let client = cluster.client().await;
     let lock_client = client.lock_client();
 
-    let lease_id = client
+    let lease_id_1 = client
         .lease_client()
         .grant(LeaseGrantRequest::new(1))
         .await?
         .id;
     let _res = lock_client
-        .lock(LockRequest::new("test").with_lease(lease_id))
+        .lock(LockRequest::new("test").with_lease(lease_id_1))
         .await?;
+    let lease_id_2 = client
+        .lease_client()
+        .grant(LeaseGrantRequest::new(DEFAULT_SESSION_TTL))
+        .await?
+        .id;
 
     let res = timeout(
         Duration::from_secs(3),
-        lock_client.lock(LockRequest::new("test")),
+        lock_client.lock(LockRequest::new("test").with_lease(lease_id_2)),
     )
     .await??;
     assert!(res.key.starts_with(b"test"));
