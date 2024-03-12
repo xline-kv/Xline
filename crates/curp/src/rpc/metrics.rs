f13ca59a723803b9cc3ad9ea6b8c5e20617956a4
iGxnon <igxnon@gmail.com>
2024-03-12 17:25:38 +0800

    chore: rename some metrics name
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- crates/curp/src/rpc/metrics.rs
+++ crates/curp/src/rpc/metrics.rs
@@ -4,11 +4,11 @@ use utils::define_metrics;
 define_metrics! {
     "curp_p2p",
     peer_sent_bytes_total: Counter<u64> = meter()
-        .u64_counter("peer_sent_bytes_total")
+        .u64_counter("peer_sent_bytes")
         .with_description("The total number of bytes send to peers.")
         .init(),
     peer_sent_failures_total: Counter<u64> = meter()
-        .u64_counter("peer_sent_failures_total")
+        .u64_counter("peer_sent_failures")
         .with_description("The total number of send failures to peers.")
         .init(),
     peer_round_trip_time_seconds: Histogram<u64> = meter()
