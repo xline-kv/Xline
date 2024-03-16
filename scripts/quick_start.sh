4139b8720303459b665dcce73985ebd117566d54
rohansx <sharmarohan615@gmail.com>
2024-03-17 00:28:29 +0530

    Refactor: Test Framework and Update Docker Compose Configuration

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -10,6 +10,7 @@ start_environment() {
     echo "The environment is started."
 }
 
+<<<<<<< HEAD
 # Function to stop and clean up the environment
 stop_environment() {
     echo "Stopping and removing the environment..."
@@ -20,6 +21,89 @@ stop_environment() {
 # Check command line arguments
 if [[ "$1" == "stop" ]]; then
     stop_environment
+=======
+# run xline node by index
+# args:
+#   $1: index of the node
+run_xline() {
+    cmd="/usr/local/bin/xline \
+    --name node${1} \
+    --members ${MEMBERS} \
+    --storage-engine rocksdb \
+    --data-dir /usr/local/xline/data-dir \
+    --auth-public-key /mnt/public.pem \
+    --auth-private-key /mnt/private.pem \
+    --client-listen-urls=http://${SERVERS[$1]}:2379 \
+    --peer-listen-urls=http://${SERVERS[$1]}:2380,http://${SERVERS[$1]}:2381 \
+    --client-advertise-urls=http://${SERVERS[$1]}:2379 \
+    --peer-advertise-urls=http://${SERVERS[$1]}:2380,http://${SERVERS[$1]}:2381"
+
+    if [ -n "$LOG_LEVEL" ]; then
+        cmd="${cmd} --log-level ${LOG_LEVEL}"
+    fi
+
+    if [ ${1} -eq 1 ]; then
+        cmd="${cmd} --is-leader"
+    fi
+
+    docker exec -e RUST_LOG=debug -d node${1} ${cmd}
+    log::info "command is: docker exec -e RUST_LOG=debug -d node${1} ${cmd}"
+}
+
+# run cluster of xline/etcd in container
+run_cluster() {
+    log::info cluster starting
+    run_xline 1 &
+    run_xline 2 &
+    run_xline 3 &
+    wait
+    log::info cluster started
+}
+
+# run container of xline/etcd use specified image
+# args:
+#   $1: size of cluster
+run_container() {
+    log::info container starting
+    size=${1}
+    image="ghcr.io/xline-kv/xline:latest"
+    for ((i = 1; i <= ${size}; i++)); do
+        mount_point="-v ${DIR}:/mnt"
+        if [ -n "$LOG_PATH" ]; then
+            mkdir -p ${LOG_PATH}/node${i}
+            mount_point="${mount_point} -v ${LOG_PATH}/node${i}:/var/log/xline"
+        fi
+        docker run -d -it --rm --name=node${i} --net=xline_net \
+            --ip=${SERVERS[$i]} --cap-add=NET_ADMIN --cpu-shares=1024 \
+            -m=512M ${mount_point} ${image} bash &
+    done
+    docker run -d -it --rm  --name=client \
+        --net=xline_net --ip=${SERVERS[0]} --cap-add=NET_ADMIN \
+        --cpu-shares=1024 -m=512M -v ${DIR}:/mnt ghcr.io/xline-kv/etcdctl:v3.5.9 bash &
+    wait
+    log::info container started
+}
+
+# run prometheus
+run_prometheus() {
+    docker run -d -it --rm --name=prometheus --net=xline_net -p 9090:9090 \
+        --ip=${1} --cap-add=NET_ADMIN -v ${DIR}/prometheus.yml:/etc/prometheus/prometheus.yml \
+        prom/prometheus
+}
+
+if [ -z "$1" ]; then
+    stop_all
+    docker network create --subnet=172.20.0.0/24 xline_net >/dev/null 2>&1
+    log::warn "A Docker network named 'xline_net' is created for communication among various xline nodes. You can use the command 'docker network rm xline_net' to remove it after use."
+    run_container 3
+    run_cluster
+    run_prometheus "172.20.0.6"
+    echo "Prometheus starts on http://172.20.0.6:9090/graph and http://127.0.0.1:9090/graph"
+    exit 0
+elif [ "$1" == "stop" ]; then
+    stop_all
+    exit 0
+>>>>>>> ef29834 (chore: mount LOG_PATH as xline container log dir)
 else
     start_environment
 fi
--- scripts/validation_test.sh
+++ scripts/validation_test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+<<<<<<< HEAD
+=======
+DIR="$(dirname $0)"
+QUICK_START="${DIR}/quick_start.sh"
+ETCDCTL="docker exec -i client etcdctl --endpoints=http://172.20.0.3:2379,http://172.20.0.4:2379"
+LOCK_CLIENT="docker exec -i client /mnt/validation_lock_client --endpoints=http://172.20.0.3:2379,http://172.20.0.4:2379,http://172.20.0.5:2379"
+>>>>>>> ef29834 (chore: mount LOG_PATH as xline container log dir)
 
 # Define directories and Docker Compose configuration
 DIR="$(dirname "$0")"
 DOCKER_COMPOSE_FILE="${DIR}/../ci/docker-compose.yml"
 
+<<<<<<< HEAD
 # Source log functions
 source "${DIR}/log.sh"
+=======
+LOG_PATH=${DIR}/logs LOG_LEVEL=debug bash ${QUICK_START}
+source $DIR/log.sh
+>>>>>>> ef29834 (chore: mount LOG_PATH as xline container log dir)
 
 # Function to stop all services using Docker Compose
 stop() {
