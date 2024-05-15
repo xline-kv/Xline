706a00693f3371b4fb4b20ec0ef4e692bbcca01f
bsbds <69835502+bsbds@users.noreply.github.com>
2024-05-15 18:48:27 +0800

    chore: use serde_bytes for xlineapi
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- crates/xlineapi/build.rs
+++ crates/xlineapi/build.rs
@@ -1,6 +1,42 @@
 fn main() {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .field_attribute(
+            "etcdserverpb.RangeRequest.key",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.RangeRequest.range_end",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.PutRequest.key",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.PutRequest.value",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.DeleteRangeRequest.key",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.DeleteRangeRequest.range_end",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.Compare.key",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.Compare.range_end",
+            "#[serde(with = \"serde_bytes\")]",
+        )
+        .field_attribute(
+            "etcdserverpb.Compare.target_union.value",
+            "#[serde(with = \"serde_bytes\")]",
+        )
         .compile(
             &[
                 "proto/src/kv.proto",
