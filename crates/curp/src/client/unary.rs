a13285164f404b42c6501ca049f021658e7e91be
bsbds <69835502+bsbds@users.noreply.github.com>
2024-05-27 21:42:07 +0800

    fix: node urls validation in raw curp
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- crates/curp/src/client/unary.rs
+++ crates/curp/src/client/unary.rs
@@ -116,6 +116,25 @@ impl<C: Command> Unary<C> {
         self.last_sent_seq
             .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
     }
+
+    /// Validates the member list, returns `true` if all member has been publised
+    fn validates_members(&self, members: &[Member]) -> bool {
+        if members.is_empty() {
+            return false;
+        }
+        for member in members {
+            let urls = if self.state.is_raw_curp() {
+                &member.peer_urls
+            } else {
+                &member.client_urls
+            };
+            if urls.is_empty() {
+                debug!("node {} not published yet", member.id());
+                return false;
+            }
+        }
+        true
+    }
 }
 
 impl<C: Command> Unary<C> {
@@ -255,20 +274,6 @@ impl<C: Command> ClientApi for Unary<C> {
     /// Send fetch cluster requests to all servers
     /// Note: The fetched cluster may still be outdated if `linearizable` is false
     async fn fetch_cluster(&self, linearizable: bool) -> Result<FetchClusterResponse, Self::Error> {
-        /// Checks the member list, returns `true` if all member has been published
-        fn check_members(members: &[Member]) -> bool {
-            if members.is_empty() {
-                return false;
-            }
-            for member in members {
-                if member.client_urls.is_empty() {
-                    debug!("new node {} not published yet", member.id());
-                    return false;
-                }
-            }
-            true
-        }
-
         let timeout = self.config.wait_synced_timeout;
         if !linearizable {
             // firstly, try to fetch the local server
@@ -329,14 +334,14 @@ impl<C: Command> ClientApi for Unary<C> {
                 match max_term.cmp(&inner.term) {
                     Ordering::Less => {
                         max_term = inner.term;
-                        if check_members(&inner.members) {
+                        if self.validates_members(&inner.members) {
                             res = Some(inner);
                         }
                         // reset ok count to 1
                         ok_cnt = 1;
                     }
                     Ordering::Equal => {
-                        if check_members(&inner.members) {
+                        if self.validates_members(&inner.members) {
                             res = Some(inner);
                         }
                         ok_cnt += 1;
