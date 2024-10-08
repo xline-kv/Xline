8278f1bb813cd9b0b21dfc6b783d036c983bfc26
bsbds <69835502+bsbds@users.noreply.github.com>
2024-10-08 20:29:47 +0800

    test: fix duplicate node id
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- crates/xline-client/tests/it/member.rs
+++ crates/xline-client/tests/it/member.rs
@@ -9,8 +9,8 @@ async fn learner_add_and_remove_are_ok() -> Result<()> {
     let (_cluster, client) = get_cluster_client().await.unwrap();
     let client = client.member_client();
 
-    let node1 = Node::new(1, "n1", vec!["10.0.0.4:2380"], vec!["10.0.0.4.2379"]);
-    let node2 = Node::new(2, "n2", vec!["10.0.0.5:2380"], vec!["10.0.0.5.2379"]);
+    let node1 = Node::new(11, "n1", vec!["10.0.0.4:2380"], vec!["10.0.0.4.2379"]);
+    let node2 = Node::new(12, "n2", vec!["10.0.0.5:2380"], vec!["10.0.0.5.2379"]);
     client
         .add_learner(vec![node1, node2])
         .await
@@ -18,7 +18,7 @@ async fn learner_add_and_remove_are_ok() -> Result<()> {
 
     // Remove the previously added learners
     client
-        .remove_learner(vec![1, 2])
+        .remove_learner(vec![11, 12])
         .await
         .expect("failed to remove learners");
 
